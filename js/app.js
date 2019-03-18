35e7a133cdb402eabcb0092c15fa6c365da32837
Deepa Kiwne <deepa.kiwne@gmail.com>
2019-03-18 01:16:11 -0700

    players and enemys are added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -24,12 +26,40 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-princess-girl.png';
+    this.x = x;
+    this.y = y;
+};
+
+Player.prototype.update = function () {
+    console.log('inside update');
+};
+
+Player.prototype.render = function () {
+    console.log('inside render');
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function () {
+    console.log('inside handleinput');
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+let allEnemies = []
+let player = new Player(200, 330);
+
+allEnemies.push(new Enemy(100,100));
+allEnemies.push(new Enemy(150,100));
+allEnemies.push(new Enemy(200,150));
 
 
 // This listens for key presses and sends the keys to your
