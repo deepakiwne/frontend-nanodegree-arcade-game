5e0a0b741578aa84c2806ec0576d96b5126a3ba2
Deepa Kiwne <deepa.kiwne@gmail.com>
2019-03-18 20:58:19 -0700

    basic functionality is working

--- js/app.js
+++ js/app.js
@@ -16,6 +16,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    this.x += 200 * dt;
+    if(this.x >= 800){
+ 		this.x = -200;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -32,22 +37,68 @@ var Player = function(x, y) {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/char-princess-girl.png';
+    this.sprite = 'images/char-horn-girl.png';
     this.x = x;
     this.y = y;
 };
 
 Player.prototype.update = function () {
-    console.log('inside update');
+    //console.log('inside update');
+
+    let collisionThreshold = 50;
+
+    // check collison
+    for(const enemy of allEnemies){
+    	if (Math.abs(enemy.x - this.x) <= collisionThreshold && Math.abs(enemy.y - this.y) <= collisionThreshold){
+    		console.log('collisonnnnnnnnnnnnnnn', Math.abs(enemy.x - this.x), Math.abs(enemy.y - this.y));
+    		this.x = 200; 
+    		this.y = 300;
+    	}
+    }
 };
 
 Player.prototype.render = function () {
-    console.log('inside render');
+    //console.log('inside render');
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function () {
-    console.log('inside handleinput');
+Player.prototype.handleInput = function (event) {
+    console.log(event);
+    // if(event === 'up'){
+    // 	console.log("up pressed");
+    // }
+    switch (event) {
+    	case 'up':
+    	console.log("up key");
+    	if(!(this.y <= 50)){
+    		this.y -= 80;
+    	}
+    	
+    	break;
+    	case 'down':
+    	console.log("down key");
+    	if(!(this.y >= 350)){
+    		this.y += 80;
+    	}
+    	
+    	break;
+    	case 'left':
+    	console.log("left key");
+    	if(!(this.x <= 50)){
+    		this.x -= 100;
+    	}
+    	
+    	break;
+    	case 'right':
+    	console.log("right key");
+    	if(!(this.x >= 400)){
+    		this.x += 100;
+    	}
+    	
+    	break;
+    	default:
+    	console.log('key not supported', event);
+    }
 };
 
 // Now instantiate your objects.
@@ -55,12 +106,20 @@ Player.prototype.handleInput = function () {
 // Place the player object in a variable called player
 
 let allEnemies = []
-let player = new Player(200, 330);
+let player = new Player(200, 300);
 
-allEnemies.push(new Enemy(100,100));
-allEnemies.push(new Enemy(150,100));
-allEnemies.push(new Enemy(200,150));
-
+allEnemies.push(new Enemy(15,65));
+allEnemies.push(new Enemy(130,65));
+allEnemies.push(new Enemy(250,65));
+allEnemies.push(new Enemy(370,65));
+allEnemies.push(new Enemy(15,150));
+allEnemies.push(new Enemy(130,150));
+allEnemies.push(new Enemy(250,150));
+allEnemies.push(new Enemy(370,150));
+allEnemies.push(new Enemy(15,232));
+allEnemies.push(new Enemy(130,232));
+allEnemies.push(new Enemy(250,232));
+allEnemies.push(new Enemy(370,232));
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- js/engine.js
+++ js/engine.js
@@ -173,7 +173,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-princess-girl.png'
+        'images/char-horn-girl.png'
     ]);
     Resources.onReady(init);
 
